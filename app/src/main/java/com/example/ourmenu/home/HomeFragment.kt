561ba750798a8176d0f883dc924a4b0da23c6f7b
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-07-11 14:03:03 +0900

    임시커밋
    refactor: homeFragment 패키지변

--- app/src/main/java/com/example/ourmenu/home/HomeFragment.kt
+++ app/src/main/java/com/example/ourmenu/home/HomeFragment.kt
@@ -1,10 +1,11 @@
-package com.example.ourmenu
+package com.example.ourmenu.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ourmenu.R
 import com.example.ourmenu.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
