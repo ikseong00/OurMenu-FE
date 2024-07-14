1ae900171f050560d5e748c4bcaf248716c87f70
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-07-14 22:52:21 +0900

    feat: menu-info 페이지 사진 어댑터, 아이템 생성

--- app/src/main/java/com/example/ourmenu/menu/MenuInfoFragment.kt
+++ app/src/main/java/com/example/ourmenu/menu/MenuInfoFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.example.ourmenu.R
+import com.example.ourmenu.data.HomeMenuData
 import com.example.ourmenu.databinding.FragmentMenuInfoBinding
+import com.example.ourmenu.menu.adapter.MenuInfoVPAdapter
 
 class MenuInfoFragment : Fragment() {
     lateinit var binding: FragmentMenuInfoBinding
@@ -17,6 +20,20 @@ class MenuInfoFragment : Fragment() {
     ): View? {
         binding = FragmentMenuInfoBinding.inflate(inflater, container, false)
 
+        initViewPager2Adapter()
+
         return binding.root
+    }
+
+    private fun initViewPager2Adapter() {
+        val dummyItems = ArrayList<String>()
+        for (i in 1..6) {
+            dummyItems.add(
+                "1"
+            )
+
+            binding.vpMenuInfoMenuImage.adapter = MenuInfoVPAdapter(dummyItems)
+            binding.vpMenuInfoMenuImage.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        }
     }
 }
