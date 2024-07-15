81a9fd619c7ee114760e5484d56f549b2d43c08d
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-07-15 23:57:25 +0900

    feat: 지도 화면 바텀시트 구현

--- app/src/main/java/com/example/ourmenu/map/MapFragment.kt
+++ app/src/main/java/com/example/ourmenu/map/MapFragment.kt
@@ -1,26 +1,22 @@
 package com.example.ourmenu.map
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ourmenu.R
+import androidx.fragment.app.Fragment
 import com.example.ourmenu.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
-
-    lateinit var binding : FragmentMapBinding
+    lateinit var binding: FragmentMapBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
-
         binding = FragmentMapBinding.inflate(inflater, container, false)
 
         return binding.root
-
     }
-
 }
