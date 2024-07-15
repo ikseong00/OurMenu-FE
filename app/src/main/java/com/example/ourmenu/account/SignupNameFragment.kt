a13c34475a87ad517c3ed9277a098f6400089112
jmlee <lee00litt@gmail.com>
2024-07-15 20:21:31 +0900

    오류 해결용

---
+++ app/src/main/java/com/example/ourmenu/account/SignupNameFragment.kt
@@ -0,0 +1,26 @@
+package com.example.ourmenu.account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ourmenu.R
+import com.example.ourmenu.databinding.FragmentSignupNameBinding
+
+class SignupNameFragment : Fragment() {
+    lateinit var binding : FragmentSignupNameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignupNameBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
