535dccc5fefa61a88f66e6874b44ab569fee488b
jmlee <lee00litt@gmail.com>
2024-07-16 19:49:05 +0900

    feat : 회원가입 화면 플로우 작성

--- app/src/main/java/com/example/ourmenu/adapters/SpinnerAdapter.kt
+++ app/src/main/java/com/example/ourmenu/adapters/SpinnerAdapter.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.ourmenu.R
 import com.example.ourmenu.account.SignupEmailFragment
 import com.example.ourmenu.databinding.FragmentSignupEmailBinding
-import com.example.ourmenu.databinding.SpinnerDefaultBackgroundTextBinding
 import com.example.ourmenu.databinding.SpinnerItemBackgroundBinding
 import com.example.ourmenu.databinding.SpinnerItemBackgroundEditBinding
 import com.example.ourmenu.databinding.SpinnerItemBackgroundNullBinding
@@ -30,6 +30,10 @@ class SpinnerAdapter<T> constructor(context: Context) :
         val editbinding = SpinnerItemBackgroundEditBinding.inflate(LayoutInflater.from(context), parent, false)
         if (position == 1){
             return editbinding.root
+        }
+        if (position == 0){
+            view.requireViewById<TextView>(R.id.tv_spinner_default_background).apply { text = ""
+            hint = "텍스트" }
         }
         return view
     }
