b3a4cc75b36cf015fa4e8b37619e578539f6ae43
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-07-18 14:45:42 +0900

    feat: MenuInfoActivity 에 기본 fragment 를 MenuInfoFragment 로 설정

--- app/src/main/java/com/example/ourmenu/menuinfo/MenuInfoActivity.kt
+++ app/src/main/java/com/example/ourmenu/menuinfo/MenuInfoActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.ourmenu.R
 import com.example.ourmenu.databinding.ActivityMenuInfoBinding
 
@@ -18,6 +19,12 @@ class MenuInfoActivity : AppCompatActivity() {
         binding = ActivityMenuInfoBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.menu_info_frm, MenuInfoFragment())
+            .commitAllowingStateLoss()
+
 
     }
 }
