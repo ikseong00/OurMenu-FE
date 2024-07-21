80436761d0b2225884a895528c5291cf74c0eed5
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-07-22 02:40:48 +0900

    feat: 메뉴판 수정하기, 확인, 삭제하기 버튼 기능 구현

--- app/src/main/java/com/example/ourmenu/menu/MenuFolderFragment.kt
+++ app/src/main/java/com/example/ourmenu/menu/MenuFolderFragment.kt
@@ -23,9 +23,24 @@ class MenuFolderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        initVertOnClickListener()
+
         binding = FragmentMenuFolderBinding.inflate(layoutInflater)
 
-        // vert 버튼 클릭
+
+
+
+
+
+        return binding.root
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S) // 이거 있어야 setRenderEffect 가능
+    private fun initVertOnClickListener() {
+
+        /* vert 버튼 클릭
+        * 배경에 blur 적용, 하단 뷰 visible */
         binding.ivMenuFolderVert.setOnClickListener {
             // 배경에 blur 효과 적용
             binding.clMenuFolderBlur.setRenderEffect(
@@ -35,6 +50,48 @@ class MenuFolderFragment : Fragment() {
             binding.clMenuFolderVert.visibility = View.VISIBLE
         }
 
+        /* 메뉴판 수정하기 클릭
+        * blur 효과 제거, 하단 뷰 gone
+        * 상단 이미지 blur 적용, vert 버튼 gone
+        * 카메라 layout visible
+        * edittext 설정 */
+        binding.btnMenuFolderEdit.setOnClickListener {
+            // blur 효과 제거
+            binding.clMenuFolderBlur.setRenderEffect(null)
+            // 메뉴판 수정하기, 삭제하기, 취소 gone
+            binding.clMenuFolderVert.visibility = View.GONE
+
+            // 상단 이미지 blur 효과 적용
+            binding.ivMenuFolderMainImage.setRenderEffect(
+                RenderEffect.createBlurEffect(2f, 2f, Shader.TileMode.CLAMP)
+            )
+            // vert 버튼 gone
+            binding.ivMenuFolderVert.visibility = View.GONE
+
+            // 카메라 , textView visible
+            binding.llMenuFolderEdit.visibility = View.VISIBLE
+
+            // edittext enabled, drawable 적용
+            with(binding.etMenuFolderMainName) {
+                isEnabled = true
+                setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_pen, 0, 0, 0)
+            }
+        }
+
+        /* 삭제하기 클릭
+        * blur 효과 제거, 하단 뷰 gone
+        * TODO 삭제하기 API */
+        binding.btnMenuFolderDelete.setOnClickListener {
+            // blur 효과 제거
+            binding.clMenuFolderBlur.setRenderEffect(null)
+            // 메뉴판 수정하기, 삭제하기, 취소 gone
+            binding.clMenuFolderVert.visibility = View.GONE
+
+            // TODO 삭제버튼 클릭 API 구현
+        }
+
+        /* 취소 버튼 클릭
+        * blur 효과 제거, 하단 뷰 gone */
         binding.btnMenuFolderCancel.setOnClickListener {
             // blur 효과 제거
             binding.clMenuFolderBlur.setRenderEffect(null)
@@ -42,9 +99,25 @@ class MenuFolderFragment : Fragment() {
             binding.clMenuFolderVert.visibility = View.GONE
         }
 
+        /* 확인 클릭
+        * 상단 이미지 blur 원상 복구, vert 버튼 visible
+        * edittext 원상 복구
+        * TODO 확인 버튼 클릭 API 구현 */
+        binding.btnMenuFolderEditOk.setOnClickListener {
+            // 상단 이미지 blur 효과 적용
+            binding.ivMenuFolderMainImage.setRenderEffect(null)
+            // vert 버튼 visible
+            binding.ivMenuFolderVert.visibility = View.VISIBLE
+            // 카메라 , textView visible
+            binding.llMenuFolderEdit.visibility = View.GONE
+            // edittext 원상 복구
+            with(binding.etMenuFolderMainName) {
+                isEnabled = false
+                setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+            }
+            // TODO 확인 버튼 클릭 API 구현
 
-
-        return binding.root
+        }
 
     }
 
