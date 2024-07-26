45cef6caad102ba1baca5f63d601d916365aa2df
jmlee <lee00litt@gmail.com>
2024-07-26 23:25:57 +0900

    refactor : 글자 잘리는 문제 해결

--- app/src/main/java/com/example/ourmenu/account/SignupPwFragment.kt
+++ app/src/main/java/com/example/ourmenu/account/SignupPwFragment.kt
@@ -1,15 +1,20 @@
 package com.example.ourmenu.account
 
+import android.content.Context
+import android.content.res.Resources
 import android.os.Bundle
 import android.text.InputType
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.ourmenu.R
 import com.example.ourmenu.databinding.FragmentSignupPwBinding
+import com.example.ourmenu.databinding.ToastErrorBinding
 
 class SignupPwFragment : Fragment() {
     lateinit var binding: FragmentSignupPwBinding
@@ -22,9 +27,33 @@ class SignupPwFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignupPwBinding.inflate(inflater, container, false)
+        var toast = object {
+
+            fun createToast(context: Context, message: String): Toast? {
+                val inflater = LayoutInflater.from(context)
+                val binding: ToastErrorBinding =
+                    ToastErrorBinding.inflate(inflater,container,false)
+                binding.tvToastError.text = message
+
+                return Toast(context).apply {
+                    setGravity(Gravity.TOP or Gravity.CENTER, 0, 96.toPx())
+                    duration = Toast.LENGTH_LONG
+                    view = binding.root
+                }
+            }
+
+            private fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
+        }
         binding.btnSignupPw.setOnClickListener {
             if (binding.etSignupPasswordEnter.text.length >= 8 && binding.etSignupPasswordEnter.text.matches(Regex("[0-9||a-z|A-Z| ]*"))) {
+                if(binding.etSignupPasswordEnter.text == binding.etSignupPasswordEnterRepeat.text){
+                    parentFragmentManager.beginTransaction()
+                        .addToBackStack("SignupPw")
+                        .replace(R.id.cl_mainscreen, SignupNameFragment())
+                        .commit()
+                }else{
 
+                }
             } else {
 
             }
