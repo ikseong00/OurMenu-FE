2a409b429c426e77bf18ca852727d3e516b404bb
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-07-26 21:04:40 +0900

    design : 메뉴 폴더 가져오기 화면 완료

---
+++ app/src/main/java/com/example/ourmenu/newfolder/PostMenuFolderGetDetailFragment.kt
@@ -0,0 +1,30 @@
+package com.example.ourmenu.newfolder
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ourmenu.R
+import com.example.ourmenu.databinding.FragmentPostMenuFolderGetDetailBinding
+
+class PostMenuFolderGetDetailFragment : Fragment() {
+
+    lateinit var binding: FragmentPostMenuFolderGetDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPostMenuFolderGetDetailBinding.inflate(layoutInflater)
+
+
+
+
+
+
+
+        
+        return binding.root
+    }
+}
