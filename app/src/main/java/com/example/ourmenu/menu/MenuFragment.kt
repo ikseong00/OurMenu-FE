d620c83f818ce0bf3393a5ffb0970001d6d36346
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-07-26 18:39:38 +0900

    feat : 메뉴 수정하기 기능 완료

--- app/src/main/java/com/example/ourmenu/menu/MenuFragment.kt
+++ app/src/main/java/com/example/ourmenu/menu/MenuFragment.kt
@@ -1,6 +1,7 @@
 package com.example.ourmenu.menu
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,16 +9,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ourmenu.addMenu.AddMenuActivity
 import com.example.ourmenu.data.HomeMenuData
 import com.example.ourmenu.databinding.FragmentMenuBinding
 import com.example.ourmenu.menu.adapter.MenuRVAdapter
 import com.example.ourmenu.menu.callback.ItemTouchHelperCallback
+import com.example.ourmenu.menu.iteminterface.MenuItemClickListener
 
 //import com.example.ourmenu.menu.listener.ItemTouchHelperCallback
 
 class MenuFragment : Fragment() {
 
     lateinit var binding: FragmentMenuBinding
+    lateinit var itemClickListener: MenuItemClickListener
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,17 +31,54 @@ class MenuFragment : Fragment() {
         binding = FragmentMenuBinding.inflate(inflater, container, false)
 
 
-
-        initTouchHelper()
+        initItemListener()
+        initTouchHelperRV()
 
 
         return binding.root
 
     }
 
+    private fun initItemListener() {
+
+        // 상단 메뉴 추가
+        binding.ivMenuAddMenu.setOnClickListener {
+            // TODO API 추가
+            val intent = Intent(context, AddMenuActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 메뉴판 추가
+        binding.btnMenuAddMenuFolder.setOnClickListener {
+
+        }
+
+
+        itemClickListener = object : MenuItemClickListener {
+            override fun onMenuClick() {
+                val intent = Intent(context, MenuFolderActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun onEditClick() {
+                // MenuFolderFragment 에서 editClick() 메소드 실행
+                val intent = Intent(context, MenuFolderActivity::class.java)
+                intent.putExtra("isEdit", true)
+                startActivity(intent)
+            }
+
+            override fun onDeleteClick() {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+
+    }
+
     //
     @SuppressLint("ClickableViewAccessibility") // 이줄 없으면 setOnTouchListener 에 밑줄생김
-    private fun initTouchHelper() {
+    private fun initTouchHelperRV() {
 
         val dummyItems = ArrayList<HomeMenuData>()
         for (i in 1..6) {
@@ -51,17 +92,17 @@ class MenuFragment : Fragment() {
         }
         val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
         itemTouchHelper.attachToRecyclerView(binding.rvMenuMenuFolder)
-
-
         // 리사이클러 뷰 설정
         with(binding.rvMenuMenuFolder) {
-            adapter = MenuRVAdapter(dummyItems, requireContext(), itemTouchHelperCallback)
-
+            adapter = MenuRVAdapter(dummyItems, requireContext(), itemTouchHelperCallback).apply {
+                setOnItemClickListener(itemClickListener)
+            }
             // 다른 뷰를 건들면 기존 뷰의 swipe 가 초기화 됨
             setOnTouchListener { _, _ ->
                 itemTouchHelperCallback.removePreviousClamp(this)
                 false
             }
+
         }
     }
 
--- app/src/main/java/com/example/ourmenu/menu/adapter/MenuRVAdapter.kt
+++ app/src/main/java/com/example/ourmenu/menu/adapter/MenuRVAdapter.kt
@@ -1,36 +1,49 @@
 package com.example.ourmenu.menu.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ourmenu.data.HomeMenuData
 import com.example.ourmenu.databinding.ItemMenuFolderBinding
 import com.example.ourmenu.menu.callback.ItemTouchHelperCallback
+import com.example.ourmenu.menu.iteminterface.MenuItemClickListener
 
 class MenuRVAdapter(
     val items: ArrayList<HomeMenuData>, val context: Context,
     val itemTouchHelperCallback: ItemTouchHelperCallback
 ) : RecyclerView.Adapter<MenuRVAdapter.ViewHolder>() {
 
+    private lateinit var itemClickListener: MenuItemClickListener
+
+    fun setOnItemClickListener(onItemClickListener: MenuItemClickListener) {
+        itemClickListener = onItemClickListener
+    }
+
     inner class ViewHolder(val binding: ItemMenuFolderBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: HomeMenuData) {
             // 왼쪽으로 swipe 된 상태, 수정버튼 삭제버튼 누를 수 있음.
-            if (itemTouchHelperCallback.isEditable()) {
 
 
-                binding.clItemMenuFolderEdit.setOnClickListener {
+            binding.clItemMenuFolderEdit.setOnClickListener {
+                if (itemTouchHelperCallback.isEditable()) {
+
                     // TODO 이벤트리스너 작성 ( 인터페이스로 )
                     // TODO API 설정
+                    itemClickListener.onEditClick()
                 }
-
-                binding.clItemMenuFolderDelete.setOnClickListener {
-                    // TODO 이벤트리스너 작성 ( 인터페이스로 )
-                    // TODO API 설정
-                }
-
             }
+
+            binding.clItemMenuFolderDelete.setOnClickListener {
+                if (itemTouchHelperCallback.isEditable()) {
+                    itemClickListener.onDeleteClick()
+                    // TODO API 설정
+                }
+            }
+
+
         }
     }
 
