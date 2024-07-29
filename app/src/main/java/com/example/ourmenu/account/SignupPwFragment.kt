5ee80a7a3f7c7d964ee60887d46d15005f18230b
jmlee <lee00litt@gmail.com>
2024-07-29 11:09:43 +0900

    - feat : signupPw에서 비밀번호 조건 판별하고 에러 메세지 띄우기 - feat : signupName 넘어갈 때 계정 생성 완료 메세지 띄우기

--- app/src/main/java/com/example/ourmenu/account/SignupPwFragment.kt
+++ app/src/main/java/com/example/ourmenu/account/SignupPwFragment.kt
@@ -14,6 +14,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.example.ourmenu.R
 import com.example.ourmenu.databinding.FragmentSignupPwBinding
+import com.example.ourmenu.databinding.ToastCorrectBinding
 import com.example.ourmenu.databinding.ToastErrorBinding
 
 class SignupPwFragment : Fragment() {
@@ -42,34 +43,52 @@ class SignupPwFragment : Fragment() {
                 }
             }
 
+            fun createToastCorrect(context: Context, message: String): Toast? {
+                val inflater = LayoutInflater.from(context)
+                val binding: ToastCorrectBinding =
+                    ToastCorrectBinding.inflate(inflater,container,false)
+                binding.tvToastCorrect.text = message
+
+                return Toast(context).apply {
+                    setGravity(Gravity.TOP or Gravity.CENTER, 0, 96.toPx())
+                    duration = Toast.LENGTH_LONG
+                    view = binding.root
+                }
+            }
+
             private fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
         }
         binding.btnSignupPw.setOnClickListener {
-            if (binding.etSignupPasswordEnter.text.length >= 8 && binding.etSignupPasswordEnter.text.matches(Regex("[0-9||a-z|A-Z| ]*"))) {
-                if(binding.etSignupPasswordEnter.text == binding.etSignupPasswordEnterRepeat.text){
+            if (binding.etSignupPasswordEnter.text.length >= 8 && binding.etSignupPasswordEnter.text.matches(Regex("[a-z|A-Z]+[0-9]+"))){
+                if(binding.etSignupPasswordEnter.text.toString() == binding.etSignupPasswordEnterRepeat.text.toString()){
                     parentFragmentManager.beginTransaction()
                         .addToBackStack("SignupPw")
                         .replace(R.id.cl_mainscreen, SignupNameFragment())
                         .commit()
-                }else{
+                    toast.createToastCorrect(requireContext(),"계정 생성 완료!")?.show()
 
+                }else{
+                    toast.createToast(requireContext(),"비밀번호가 일치하지 않아요.")?.show()
+                    binding.etSignupPasswordEnter.setBackgroundResource(R.drawable.edittext_bg_error)
+                    binding.etSignupPasswordEnterRepeat.setBackgroundResource(R.drawable.edittext_bg_error)
                 }
             } else {
+                toast.createToast(requireContext(),"비밀번호 조건을 다시 확인해주세요.")?.show()
+                binding.etSignupPasswordEnter.setBackgroundResource(R.drawable.edittext_bg_error)
 
             }
-            parentFragmentManager.beginTransaction()
-                .addToBackStack("SignupPw")
-                .replace(R.id.cl_mainscreen, SignupNameFragment())
-                .commit()
         }
         var flag = true
         binding.cbSignupShowPassword.setOnClickListener {
             if (flag) {
                 binding.etSignupPasswordEnter.transformationMethod = HideReturnsTransformationMethod.getInstance()
                 binding.etSignupPasswordEnter.inputType = InputType.TYPE_CLASS_TEXT
+                binding.etSignupPasswordEnterRepeat.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                binding.etSignupPasswordEnterRepeat.inputType = InputType.TYPE_CLASS_TEXT
                 flag = false
             } else {
                 binding.etSignupPasswordEnter.transformationMethod = PasswordTransformationMethod.getInstance()
+                binding.etSignupPasswordEnterRepeat.transformationMethod = PasswordTransformationMethod.getInstance()
                 flag = true
             }
         }
