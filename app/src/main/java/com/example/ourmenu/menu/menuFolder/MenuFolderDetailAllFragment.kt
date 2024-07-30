b5be26ee997c3ab34390d46c2ea6401aba1d5653
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-07-30 12:53:37 +0900

    design : all menu page

---
+++ app/src/main/java/com/example/ourmenu/menu/menuFolder/MenuFolderDetailAllFragment.kt
@@ -0,0 +1,27 @@
+package com.example.ourmenu.menu.menuFolder
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ourmenu.R
+import com.example.ourmenu.databinding.FragmentMenuFolderDetailAllBinding
+
+class MenuFolderDetailAllFragment : Fragment() {
+
+    lateinit var binding: FragmentMenuFolderDetailAllBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentMenuFolderDetailAllBinding.inflate(layoutInflater)
+
+
+
+
+        return binding.root
+    }
+}
