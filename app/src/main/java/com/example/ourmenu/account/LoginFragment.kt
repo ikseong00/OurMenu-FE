468be8956c8c644f72a031631b47b298a0cdc423
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-08-02 23:46:10 +0900

    fix: 화면 이동할 때 키보드 내려가도록 수정

--- app/src/main/java/com/example/ourmenu/account/LoginFragment.kt
+++ app/src/main/java/com/example/ourmenu/account/LoginFragment.kt
@@ -1,11 +1,13 @@
 package com.example.ourmenu.account
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import com.example.ourmenu.MainActivity
 import com.example.ourmenu.R
@@ -31,6 +33,11 @@ class LoginFragment : Fragment() {
 
         binding.btnLoginLogin.setOnClickListener {
             val intent = Intent(activity, MainActivity::class.java)
+
+            // 화면 이동할 때 키보드 숨기기
+            val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.etLoginPassword.windowToken, 0)
+
             startActivity(intent)
         }
 
