dcf16868053f0555b4a78adb8a58eb7e1d6f972b
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-08-08 15:21:51 +0900

    refactor: errorResponse 설정

--- app/src/main/java/com/example/ourmenu/data/ErrorResponse.kt
+++ app/src/main/java/com/example/ourmenu/data/ErrorResponse.kt
@@ -1,6 +1,11 @@
 package com.example.ourmenu.data
 
 data class ErrorResponse(
+    val isSuccess: Boolean,
+    val errorResponse: ErrorData
+)
+
+data class ErrorData(
     val status: Int,
     val code: String,
     val message: String,
